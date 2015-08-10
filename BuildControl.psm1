289ff301fba41e6149cfb4d241d12e84a24ba508
Gordon Watts <gordon@gordonandpaula.net>
2015-08-10 19:34:07 +0200

    Updates

--- BuildControl.psm1
+++ BuildControl.psm1
@@ -101,7 +101,6 @@ function BuildROOTNET ($rootLoc, $rootdotnetloc, $finalbuild, $fullVersion, $log
     $buildDir = "$rootdotnetloc/Wrapper Generators"
     set-location $buildDir
     Write-Host "Building wrappers..."
-    Write-Host "Location is $($PWD.PATH)"
     & DoEverythingAtOnce\BuildROOTWrappers.bat "$rootLoc" AutoBuild "$finalbuild" $version 2>&1 | out-file "$logDir/build.log"
     
     #
