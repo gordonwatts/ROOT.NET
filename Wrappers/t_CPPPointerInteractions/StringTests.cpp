d4f65bf46d85a769607ebcc0ab4fc0bdfd3d60bc
Gordon Watts <gordon@gordonandpaula.net>
2016-02-11 23:29:20 +0100

    Added some string testing

---
+++ Wrappers/t_CPPPointerInteractions/StringTests.cpp
@@ -0,0 +1,36 @@
+#include "stdafx.h"
+
+using namespace System;
+using namespace System::Text;
+using namespace System::Collections::Generic;
+using namespace	Microsoft::VisualStudio::TestTools::UnitTesting;
+using namespace System::Runtime::InteropServices;
+
+namespace t_CPPPointerInteractions
+{
+	[TestClass]
+	public ref class UnitTest
+	{
+	public: 
+		[TestMethod]
+		void ConvertFromChar()
+		{
+			char a = 'a';
+			System::Char ^neta = gcnew System::Char(a);
+			System::Console::WriteLine(neta);
+		}
+
+		[TestMethodAttribute]
+		void ConvertToChar()
+		{
+			System::Char neta = System::Char('b');
+
+			//auto c = neta->ToChar(nullptr);
+			System::String ^str = gcnew System::String(neta,1);
+			auto trans = Marshal::StringToHGlobalAnsi(str->ToString());
+			auto cstr = static_cast<const char*>(trans.ToPointer());
+			char c = *cstr;
+			Marshal::FreeHGlobal(trans);
+		}
+	};
+}
